fd9f4948daabfca6c21e07c679b19d7febd16230
visatk <visatk29@gmail.com>
2026-05-31 11:45:22 +0600

    Update types.ts

--- src/types.ts
+++ src/types.ts
@@ -1,11 +1,10 @@
 export type Env = {
 	TELEGRAM_BOT_TOKEN: string;
 	APIRONE_ACCOUNT: string;
-	WEBHOOK_SECRET: string; // Used to secure payment webhooks
+	WEBHOOK_SECRET: string;
 	DB: D1Database;
 };
 
-// Custom variables for Hono Context
 export type Variables = {
 	user: {
 		id: number;
