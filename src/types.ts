0bfbfc0de61f816bcbb656eb91e71154716bd6cb
visatk <visatk29@gmail.com>
2026-05-31 10:45:18 +0600

    Update types.ts

--- src/types.ts
+++ src/types.ts
@@ -1,7 +1,7 @@
 export type Env = {
 	TELEGRAM_BOT_TOKEN: string;
-	APIRONE_ACCOUNT: string; // Your Apirone Account ID
-	WEBHOOK_SECRET: string;  // Secret to secure your webhooks
-	APP_URL: string;         // e.g., https://your-worker.workers.dev
+	APIRONE_ACCOUNT: string; 
+	WEBHOOK_SECRET: string;  
+	APP_URL: string;         
 	DB: D1Database;
 };
